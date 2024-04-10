4fe0958478ecde7e7c1f4e6cf897c90f39dde4aa
sidTsui <smt4601@uncw.edu>
2024-04-10 02:58:34 +0000

    publisher code

---
+++ scripts/init.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+#source: in class provided recourses: https://github.com/hsaeidi-uncw/ur5e_control.git
+#Sidney Tsui
+#Report 2
+import rospy
+import math
+# import the messages for reading the joint positions and sending joint commands
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from std_msgs.msg import Header
+
+##outline from ur5e_control.git repo, hsaeidi-uncw, report 1
+
+if __name__ == '__main__':
+	# initialize the node
+	rospy.init_node('manual_initialization', anonymous = True)
+	# add a publisher for sending joint position commands
+	pos_pub = rospy.Publisher('/pos_joint_traj_controller/command', JointTrajectory, queue_size = 10)
+	# set a 10Hz frequency for this loop
+	loop_rate = rospy.Rate(10)
+
+	# define a joint trajectory variable for sending the control commands
+	pos_cmd = JointTrajectory()
+	pos_cmd_point = JointTrajectoryPoint()
+	# just a quick solution to complete the message template
+	pos_cmd.joint_names.append('elbow_joint')
+	pos_cmd.joint_names.append('shoulder_lift_joint')
+	pos_cmd.joint_names.append('shoulder_pan_joint')
+	pos_cmd.joint_names.append('wrist_1_joint')
+	pos_cmd.joint_names.append('wrist_2_joint')
+	pos_cmd.joint_names.append('wrist_3_joint')
+	
+	# initialize the position command to zero
+	for joint_no in range(6):
+		pos_cmd_point.positions.append(0.0)
+	# set the ideal time to destination
+	pos_cmd_point.time_from_start = rospy.Duration(1.0) # here one second 
+	# just change the value of the command for the second joint
+	pos_cmd_point.positions[1] = math.pi/4
+	# just change the value of the command for the elbow joint
+	pos_cmd_point.positions[2] = -math.pi/4
+	# add the trajectory point to the command
+	pos_cmd_point.positions[3] = -math.pi/2 #used trial and error to move joint to appropriate place
+	#setting initial positions
+	pos_cmd_point.positions[4] = -math.pi/2#used trial and error for correct values
+	pos_cmd.points.append(pos_cmd_point)
+	# define a message header	
+
+	
+	while not rospy.is_shutdown():
+		header = Header()
+		# update the header with the most recent time stamp
+		header.stamp = rospy.Time.now()
+		# use the most recent header in the position command message
+		pos_cmd.header = header
+		# publish the message
+		pos_pub.publish(pos_cmd)# wait for 0.1 seconds until the next loop and repeat
+		loop_rate.sleep()
---
+++ scripts/planner.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+#source: https://github.com/hsaeidi-uncw/ur5e_control.git
+#Sidney Tsui
+#Report 2
+
+import rospy
+import math
+
+#outline from ur5e_control.git by hsaeidi-uncw
+
+# import the plan message
+from ur5e_control.msg import Plan
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
+from robot_vision_lectures.msg import SphereParams
+#imports for camera to base transformations
+import tf2_ros
+from tf.transformations import *
+from geometry_msgs.msg import Quaternion
+import tf2_geometry_msgs
+
+raw_x = 0
+raw_y = 0
+raw_z = 0
+rad = 0
+curr = [0,0,0,0,0,0]
+
+#processes data from ROS topic about the sphere
+
+def get_sphere(data):
+#global varrs can be changed through the entire node
+	global raw_x, raw_y, raw_z, rad
+	#assiging values to global varrs 
+	raw_x = data.xc
+	raw_y = data.yc
+	raw_z = data.zc
+	rad = data.radius
+    
+   	def add_pt(linear_x, linear_y, linear_z, angular_x, angular_y, angular_z, plan):
+        add_point = Twist()#msg obj create
+        add_point.linear.x = linear_x
+        add_point.linear.y = linear_y
+        add_point.linear.z = linear_z
+        add_point.angular.x = angular_x
+        add_point.angular.y = angular_y
+        add_point.angular.z = angular_z
+        add.points.append(add_point)#add plan_point to plan
+		
+	
+	def get_curr(data):
+		global curr#set as global to be updated later
+		curr[0] = data.linear.x#update here
+		curr[1] = data.linear.y
+		curr[2] = data.linear.z
+		curr[3] = data.angular.x
+		curr[4] = data.angular.y
+		curr[5] = data.angular.z
+
+if __name__ == '__main__':
+	# initialize the node
+	rospy.init_node('planner', anonymous = True)
+	rospy.Subscriber('sphere_params', SphereParams, get_sphere_data)
+	rospy.Subscriber('/ur5e/toolpose', Twist, get_curr)
+ 	#publisher
+    plan_pub = rospy.Publisher('/plan', Plan, queue_size=10)
+	# set a 10Hz frequency for this loop
+	loop_rate = rospy.Rate(10)
+
+	# define a plan variable
+	plan = Plan()
+	tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+	q_rot = Quaternion()
+	#source: lect 20, slide 11-12, https://wiki.ros.org/tf2/Tutorials
+	while not rospy.is_shutdown():
+		try:
+			trans = tfBuffer.lookup_transform("base", "camera_color_optical_frame", rospy.Time())
+			#create message
+			frame_pt = tf2_geometry_msgs.PointStamped()#init obj
+    		frame_pt.header.frame_id = 'camera_color_optical_frame'#set ID
+    		frame_pt.header.stamp = rospy.get_rostime()#timestamp
+    		#assign points from stored values
+    		frame_pt.point.x = raw_x
+    		frame_pt.point.y = raw_y
+    		frame_pt.point.z = raw_z
+   	 		#point to base frame
+    		new_pt = tfBuffer.transform(frame_pt, 'base', rospy.Duration(1.0))#camera to base
+    		#get coord
+    		base = tfBuffer.transform(ptcam,'base', rospy.Duration(1.0))
+			x = base.point.x
+			y = base.point.y
+			z = base.point.z
+  	  		radius = rad
+  	  		q_rot = trans.transform.translation
+  	  		print("x: ", x, "\n", "y: ", y,"\n", "z: ", z, "\n", "radius: ", radius)
+		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+			print('Frames not available!!!')
+			loop_rate.sleep()
+			continue
+			
+		
+		# publish the plan
+		plan_pub.publish(plan)
+		# wait for 0.1 seconds until the next loop and repeat
+		loop_rate.sleep()
